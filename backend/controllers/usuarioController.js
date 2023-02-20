fbba0a1c349b6261f3f6966442f7675e70052dc7
Jorge Meza Bello <jorberiver@gmail.com>
2023-02-19 23:57:02 -0600

    Prevenir usuarios duplicados

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -2,8 +2,16 @@
 import Usuario from "../models/Usuario.js"
 
 const registrar = async (req, res) => {
+    const { email } = req.body;
+    const existeUsuario = await Usuario.findOne({email});
+    if(existeUsuario){
+        console.log(`Usuario ya existente ${existeUsuario}`);
+        const error = new Error("Usuario ya registrado");
+        return res.status(400).json({msg: error.message});
+    }
     try {
         const usuario = new Usuario(req.body);
+
         const usuarioGuardado = await usuario.save();
     } catch (error) {
         console.log(error);
