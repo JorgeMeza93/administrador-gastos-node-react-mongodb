b580645813243460fd588262b3b1a183564e6513
Jorge Meza Bello <jorberiver@gmail.com>
2023-02-22 14:24:14 -0600

    Cambiar password listo

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,5 +1,7 @@
 import Usuario from "../models/Usuario.js"
 import generarJWT from "../helpers/generarJWT.js";
+import generarId from "../helpers/generarId.js";
+import { json } from "express";
 
 const registrar = async (req, res) => {
     const { email } = req.body;
@@ -58,7 +60,54 @@ const login = async (req, res) => {
 }
 
 const verPerfil = (req, res) => {
-   res.json(req.usuario);
+    const { usuario } = req
+    res.json(req.usuario);
 }
 
-export { registrar, confirmar, login, verPerfil }
+const olvidePassword = async (req, res) => {
+    const { email } = req.body;
+    const existe = await Usuario.findOne({ email });
+    if(!existe){
+        const error = new Error("El usuario no existe");
+        return res.status(400).json({ msg: error.message });
+    }
+    try {
+        existe.token = generarId();
+        await existe.save();
+        res,json({ msg: "Hemos enviado un email con las instrucciones"});
+    } catch (error) {
+        
+    }
+}
+
+const comprobarToken = async(req, res) => {
+    const { token } = req.params;
+    const tokenValido = await Usuario.findOne({ token });
+    if(tokenValido){
+        res.json({ msg: "Usuario válido, el token si existe" })
+    }
+    else{
+        const error = new Error("Token no válido");
+        return res.status(400).json({ msg: error.message })
+    }
+}
+
+const nuevoPassword = async (req, res) => {
+    const { token  } = req.params;
+    const { password } = req.body;
+    const resultado = await Usuario.findOne({ token });
+    if(!resultado){
+        const error = new Error("Ha ocurrido un error");
+        return res.status(400).json({ msg: error.message });
+    }
+    try {
+        resultado.token = null;
+        resultado.password = password;
+        await resultado.save();
+        res.json({ msg: "El password ha sido cambiado correctamente" });
+    } catch (error) {
+        console.log(err);
+    }
+}
+
+export { registrar, confirmar, login, verPerfil, olvidePassword, comprobarToken, nuevoPassword }
--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,13 +1,19 @@
 import express from "express";
-import { registrar, confirmar, login, verPerfil } from "../controllers/usuarioController.js";
+import { registrar, confirmar, login, verPerfil, olvidePassword, comprobarToken, nuevoPassword } from "../controllers/usuarioController.js";
 import { checkAuth } from "../middlewares/middlewareAuth.js";
 
 const router = express.Router();
 
+// Área Pública
 router.post("/registrar", registrar);
 router.get("/confirmar/:token", confirmar);
 router.post("/login", login);
-router.get("/perfil", checkAuth, verPerfil)
+router.post("/olvide-password", olvidePassword);
+router.get("/olvide-password/:token", comprobarToken);
+router.post("/olvide-password/:token", nuevoPassword)
+
+// Área privada
+router.get("/perfil", checkAuth, verPerfil);
 
 
 export default router;
