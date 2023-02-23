ab587e4760ef1a948c7de219213f7a5c976a83ae
Jorge Meza Bello <jorberiver@gmail.com>
2023-02-23 02:57:19 -0600

    Nuevo gasto y obtener gastos listo

--- backend/controllers/gastosController.js
+++ backend/controllers/gastosController.js
@@ -4,15 +4,18 @@ const agregarGasto = async (req, res) => {
    try {
      const gasto = new Gasto(req.body);
      gasto.persona = req.usuario._id;
-     const gastoGuardado = await Gasto.save();
-     res.json({ msg: "Guardado" });
-   } catch (error) {
-    
+     const gastoGuardado = await gasto.save();
+     res.json({ gastoGuardado, msg: "Gasto guardado" });
+   }
+   catch (error) {
+     console.log(error) 
    }
 }
 
 const obtenerGastos = async (req, res) => {
-
+  console.log(req)
+  const gastos = await Gasto.find().where("persona").equals(req.usuario);
+  res.json(gastos);
 }
 
 export { agregarGasto, obtenerGastos }
