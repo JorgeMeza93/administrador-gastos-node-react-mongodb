54e320b263419cc7d6bfad2cf33833dc90f85f44
Jorge Meza Bello <jorberiver@gmail.com>
2023-02-27 21:45:02 -0600

    Rutas creadas y componentes definidos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./layout/Auth";
 import Login from "./paginas/Login";
+import Registrar from "./paginas/Registrar";
+import OlvidePassword from "./paginas/OlvidePassword";
+import ConfirmarCuenta from "./paginas/ConfirmarCuenta";
 
 function App() {
 
@@ -9,6 +12,9 @@ function App() {
     <Routes>
       <Route path="/" element={ <Auth/> }>
         <Route index element={ <Login/> } />
+        <Route path="registrar" element={ <Registrar/> }/>
+        <Route path="olvide-password" element={ <OlvidePassword/> } />
+        <Route path="confirmar/:id" element={ <ConfirmarCuenta/> } />
       </Route>
     </Routes>
    </BrowserRouter>
--- frontend/src/layout/Auth.jsx
+++ frontend/src/layout/Auth.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 const Auth = () => {
   return (
     <Fragment>
-        <h1 className='text-center'>Desde AUTH</h1>
+        <h1 className='text-center'>Administrador de Gastos</h1>
         <Outlet/>
     </Fragment>
   )
