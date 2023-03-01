a0d78fda6e06f92f030e75beac1fa408c2c65f7a
Jorge Meza Bello <jorberiver@gmail.com>
2023-02-28 23:00:26 -0600

    Validacion formulario de registro

--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -1,7 +1,27 @@
-import React, { Fragment} from 'react';
+import React, { Fragment, useState} from 'react';
 import { Link } from 'react-router-dom';
 
 const Registrar = () => {
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repetirPassword, setRepetirPassword] = useState("");
+  const [telefono, setTelefono] = useState("")
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    if([nombre.trim(), email.trim(), password.trim(), repetirPassword.trim(), telefono.trim()].includes("")){
+        console.log("Hay campos vacíos");
+        return
+    }
+    if(password !== repetirPassword){
+        console.log("Los passwords no son inguales");
+    }
+    if(password.length < 8 ){
+        console.log("El password es muy corto, agrega minímo 8 caracteres");
+    }
+  }
+
   return (
     <Fragment>
         <div>
@@ -9,26 +29,26 @@ const Registrar = () => {
             <h2 className='text-sky-500 font-bold text-3xl'>Es totalmente gratuito y no te toma más de cinco minutos</h2>
         </div>
         <div className='mt-20 shadow-lg px-5 py-10'>
-            <form className='w-5/6 m-auto'>
+            <form className='w-5/6 m-auto' onSubmit={handleSubmit} >
                 <div className='flex justify-around items-center m-5'>
                     <label className='font-bold w-1/4'>Nombre</label>
-                    <input type="text" placeholder='Tu nombre' className='border-none p-2 bg-gray-100 w-3/4 rounded-md'/>
+                    <input type="text" placeholder='Tu nombre' className='border-none p-2 bg-gray-100 w-3/4 rounded-md' value={nombre} onChange={ e => setNombre(e.target.value) }/>
                 </div>
                 <div className='flex justify-around items-center m-5'>
                     <label className='font-bold w-1/4'>Email</label>
-                    <input type="email" placeholder='Email de registro' className='border-none p-2 bg-gray-100 w-3/4 rounded-md'/>
+                    <input type="email" placeholder='Email de registro' className='border-none p-2 bg-gray-100 w-3/4 rounded-md' value={email} onChange={ e => setEmail(e.target.value) } />
                 </div>
                 <div className='flex justify-around items-center m-5'>
                     <label className='font-bold w-1/4'>Teléfono</label>
-                    <input type="tel" placeholder='Número telefónico' className='border-none p-2 bg-gray-100 w-3/4 rounded-md'/>
+                    <input type="tel" placeholder='Número telefónico' className='border-none p-2 bg-gray-100 w-3/4 rounded-md' value={telefono} onChange={ e => setTelefono(e.target.value)} />
                 </div>
                 <div className='flex justify-around items-center m-5'>
                     <label className='font-bold w-1/4'>Password</label>
-                    <input type="password" placeholder='Contraseña de la cuenta' className='border-none p-2 bg-gray-100 w-3/4 rounded-md'/>
+                    <input type="password" placeholder='Contraseña de la cuenta' className='border-none p-2 bg-gray-100 w-3/4 rounded-md' value={password} onChange={ e => setPassword(e.target.value)} />
                 </div>
                 <div className='flex justify-around items-center m-5'>
                     <label className='font-bold w-1/4'>Repetir Password</label>
-                    <input type="password" placeholder='Repite password' className='border-none p-2 bg-gray-100 w-3/4 rounded-md'/>
+                    <input type="password" placeholder='Repite password' className='border-none p-2 bg-gray-100 w-3/4 rounded-md' value={repetirPassword} onChange={ e => setRepetirPassword(e.target.value)} />
                 </div>
                 <input type="submit" value="Iniciar Sesión" className='bg-emerald-500 text-white font-bold uppercase hover:cursor-pointer hover:bg-emerald-700 w-full py-2
                     rounded-md px-10 mt-10'/>
