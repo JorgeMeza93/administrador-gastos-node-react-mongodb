8b0e7a05a7afaafa9fd7ea1ed6e9c9a98f8b44b6
Jorge Meza Bello <jorberiver@gmail.com>
2023-03-02 01:24:37 -0600

    Cambios añadidos a crear token y envio de email de confirmacion

--- backend/helpers/emailRegistro.js
+++ backend/helpers/emailRegistro.js
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
 
-const emailRegistro = (datos) => {
+const emailRegistro = async (datos) => {
     var transport = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
@@ -9,7 +9,19 @@ const emailRegistro = (datos) => {
             pass: process.env.EMAIL_PASSWORD
         }
     });
-
+    const { nombre, token, email } = datos;
+    const infoRegistro = await transport.sendMail({
+        from: "Administrador de Gastos",
+        to: email,
+        subject: "Comprueba tu cuenta en Administrador de Gastos",
+        text: "Comprueba tu cuenta",
+        html: `<p>Hola ${nombre}, comprueba tu cuenta en Administrador de Gastos</p>
+            <p>Tu cuenta ya está lista sólo haz clicke en el siguiente enlace: 
+            <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar cuenta</a> </p>
+            <p>Si tú no has creado esta cuenta puedes ignorar el mensaje </p>
+        `
+    })
+    console.log("Mensaje enviado: %s", infoRegistro.messageId);
 }
 
 export default emailRegistro;
--- backend/helpers/generarId.js
+++ backend/helpers/generarId.js
@@ -1,5 +1,12 @@
 const generarId = () => {
-    return (100 * Math.random()).toString(32) + Date.now().toString(32);
+    const cadena = (100 * Math.random()).toString(32) + Date.now().toString(32);
+    let token;
+    for(let i = 0; i < cadena.length; i++){
+        if(cadena.charAt(i) === "."){
+            token = cadena.replace(".", "0");
+        }
+    }
+    return token;
 }
 
 export default generarId;
