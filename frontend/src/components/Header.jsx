201b5524e64a7b7f8a60680f23a9aabae628312e
Jorge Meza Bello <jorberiver@gmail.com>
2023-03-06 23:54:12 -0600

    Header listo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className='py-10 bg-emerald-600 '>
-        <div className='container mx-auto flex justify-between items-center'>
-            <h1 className='font-bold text-2xl text-indigo-100'>Administrador de Gastos</h1>
-            <nav className='flex gap-4'>
+        <div className='container mx-auto flex flex-col lg:flex-row justify-between items-center'>
+            <h1 className='font-bold text-2xl text-indigo-100 text-center'>Administrador de Gastos</h1>
+            <nav className='flex flex-col lg:flex-row gap-4 mt-5 lg:mt-0 items-center'>
                 <Link to="/admin" className='text-white text-sm uppercase font-bold' >Gastos</Link>
                 <Link to="/perfil" className='text-white text-sm uppercase font-bold' >Perfil</Link>
                 <button type='button' className='text-white text-sm uppercase font-bold' >Cerrar Sesión</button>
--- frontend/src/layout/RutaProtegida.jsx
+++ frontend/src/layout/RutaProtegida.jsx
@@ -13,7 +13,11 @@ const RutaProtegida = () => {
   return (
     <Fragment>
       <Header/>
-        { auth?._id ? <Outlet/> : <Navigate to="/" /> }
+        { auth?._id ? (
+          <main className='container mx-auto mt-10'>
+            <Outlet/>
+          </main>
+          ) : <Navigate to="/" /> }
       <Footer/>
     </Fragment>
   )
