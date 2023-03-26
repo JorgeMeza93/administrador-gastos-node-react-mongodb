1a22deabdfc804a8627306592cfeaf7597a07a63
Jorge Meza Bello <jorberiver@gmail.com>
2023-03-25 22:52:15 -0600

    Verificacion de campos y traer datos a los campos

--- frontend/src/paginas/EditarPerfil.jsx
+++ frontend/src/paginas/EditarPerfil.jsx
@@ -1,13 +1,26 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import AdminNav from '../components/AdminNav';
 import useAuth from "../hooks/useAuth";
+import Alerta from "../components/Alerta";
 
 const EditarPerfil = () => {
   const { auth } = useAuth();
   const [perfil, setPerfil] = useState({});
+  const [alerta, setAlerta] = useState({});
+
   useEffect( () => {
     setPerfil(auth);
   }, [auth])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { nombre, email, telefono } = perfil;
+    if([nombre.trim(), email.trim(), telefono.trim()].includes("")){
+        setAlerta({ msg: "Todos los campos son obligatorios"})
+        return
+    }
+  }
+  const { msg } = alerta
   return (
     <Fragment>
         <AdminNav/>
@@ -15,7 +28,8 @@ const EditarPerfil = () => {
         <p className='text-emerald-500 font-bold text-xl mt-5 mb-10 text-center' >Modifica tu <span className='text-sky-500'>Información</span></p>
         <div className='flex justify-center'>
             <div className='w-full md:w-1/2 bg-white shadow rounded-lg p-5'>
-                <form>
+                { msg && <Alerta /> }
+                <form onSubmit={handleSubmit}>
                     <div className='my-3'>
                         <label className='uppercase font-bold text-gray-600'>Nombre</label>
                         <input type="text" className='border bg-gray-50 w-full p-2 mt-5 rounded-lg' name='nombre' value={perfil.nombre || "" } onChange={ e => setPerfil({
@@ -25,11 +39,17 @@ const EditarPerfil = () => {
                     </div>
                     <div className='my-3'>
                         <label className='uppercase font-bold text-gray-600'>Email</label>
-                        <input type="email" className='border bg-gray-50 w-full p-2 mt-5 rounded-lg' name='email' />
+                        <input type="email" className='border bg-gray-50 w-full p-2 mt-5 rounded-lg' name='email' value={perfil.email || "" } onChange={ e => setPerfil({
+                            ...perfil,
+                            [e.target.name]: e.target.value
+                        })} />
                     </div>
                     <div className='my-3'>
                         <label className='uppercase font-bold text-gray-600'>Teléfono</label>
-                        <input type="tel" className='border bg-gray-50 w-full p-2 mt-5 rounded-lg' name='telefono' />
+                        <input type="tel" className='border bg-gray-50 w-full p-2 mt-5 rounded-lg' name='telefono' value={perfil.telefono || "" } onChange={ e => setPerfil({
+                            ...perfil,
+                            [e.target.name]: e.target.value
+                        }) } />
                     </div>
                     <input type="submit" value="Guardar Cambios" className='bg-emerald-500 px-10 py-3 font-bold text-white rounded-lg uppercase w-full mt-5 hover:bg-emerald-600' />
                 </form>
